876d0cfe8e166d4d6808b23e70bddf8191932c10
kannan-ravi <kannanravindran00kr@gmail.com>
2024-03-13 00:15:32 +0530

    Data sending from frontend for contact form completed.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,5 @@
-import PageLayout from "./routes/PageLayout";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
-import Home from "./pages/Home";
-import Admin from "./pages/admin/dashboard/Admin";
-import Login from "./pages/admin/login/Login";
+import { RouterProvider } from "react-router-dom";
 
-import { Routes, Route, BrowserRouter, RouterProvider } from "react-router-dom";
-import Profile from "./pages/admin/profile/Profile";
-import Posts from "./pages/admin/posts/Posts";
-import EditProfile from "./pages/admin/profile/EditProfile";
-import PrivateRoutes from "./routes/ProtectedRoute";
 import router from "./router";
 function App() {
   return <RouterProvider router={router} />;
---
+++ client/src/pages/home/ContactForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import useFetch from "../../hooks/useFetch";
+
+const ContactForm = () => {
+  const { fetchData } = useFetch();
+
+  const [contactFormData, setContactFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    setContactFormData({ ...contactFormData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (
+      contactFormData.name &&
+      contactFormData.email &&
+      contactFormData.message
+    ) {
+      try {
+        const url = "contact-attempt";
+        const data = await fetchData(url, contactFormData);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Please fill all the fields");
+    }
+  };
+  return (
+    <section id="contact" className="flex items-center min-h-screen py-10">
+      <div className="w-full max-w-4xl px-4 mx-auto">
+        <h2 className="mb-6 text-4xl font-bold text-slate-300 contact-heading">
+          Contact Me
+        </h2>
+        <p className="mt-3 text-gray-400 contact-description">
+          I'd love to hear from you! Feel free to reach out to me using the form
+          below.
+        </p>
+        <form className="grid gap-4 mt-6 contact-form" onSubmit={handleSubmit}>
+          <input
+            className="py-2 px-4 rounded-md bg-[#2D3748] text-white w-full"
+            placeholder="Your Name"
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={contactFormData.name && contactFormData.name}
+          />
+          <input
+            className="py-2 px-4 rounded-md bg-[#2D3748] text-white"
+            placeholder="Your Email"
+            type="email"
+            name="email"
+            onChange={handleChange}
+            value={contactFormData.email && contactFormData.email}
+          />
+          <textarea
+            className="py-2 px-4 rounded-md bg-[#2D3748] text-white h-32"
+            placeholder="Your Message"
+            name="message"
+            onChange={handleChange}
+            value={contactFormData.mesage && contactFormData.mesage}
+          />
+          <button
+            className="py-2 px-4 rounded-md bg-[#4A5568] text-white hover:opacity-85 duration-200"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ContactForm;
