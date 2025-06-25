c15e6e46514fb5326a95c2f1ee81cff58dc62cf2
kannan-ravi <kannanravindran00kr@gmail.com>
2025-06-25 23:32:03 +0530

    Completed the Nav hovering design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-amber-50 dark:bg-black`}
       >
           {children}
       </body>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,29 +1,75 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import ThemeButton from "./ui/ThemeButton";
 import Link from "next/link";
+import { AiFillHome, AiFillPhone } from "react-icons/ai";
+import { GiWhiteBook } from "react-icons/gi";
 
 const Nav = () => {
+  const [hoveredItem, setHoveredItem] = useState<null | string>(null);
+  const hoverRef = useRef(null);
+  const [hoverStyle, setHoverStyle] = useState({});
+
+  useEffect(() => {
+    if (hoveredItem && hoverRef.current) {
+      const itemElement = hoverRef.current.querySelector(`#${hoveredItem}`);
+      if (itemElement) {
+        setHoverStyle({
+          width: itemElement.offsetWidth,
+          left: itemElement.offsetLeft,
+          opacity: 1,
+        });
+      }
+    } else {
+      setHoverStyle({ opacity: 0 });
+    }
+  }, [hoveredItem]);
 
   return (
-    <nav className="fixed bottom-6 left-1/2 -translate-1/2 border rounded-full overflow-hidden">
-      <ul className="flex items-center px-8 gap-4 justify-between">
-        <li className="min-h-12 flex items-center">
-          <Link href="" className="tracking-wide">
-            Profile
+    <nav className="fixed bottom-6 left-1/2 -translate-x-1/2 border rounded-full overflow-hidden dark:border-amber-50">
+      <ul
+        ref={hoverRef}
+        className="flex items-center justify-between relative"
+        onMouseLeave={() => setHoveredItem(null)}
+      >
+        <div
+          className="absolute top-0 h-full bg-red-50 dark:bg-slate-800 transition-all duration-300 ease-in-out -z-10"
+          style={hoverStyle}
+        ></div>
+
+        <li
+          className="min-h-16 flex items-center dark:text-white relative z-10 ps-8 pe-6"
+          id="home"
+          onMouseEnter={() => setHoveredItem("home")}
+        >
+          <Link href="/" className="tracking-wide text-background">
+            <AiFillHome className="text-2xl" />
           </Link>
         </li>
-        <li className="min-h-12 flex items-center">
+        <li
+          className="min-h-12 flex items-center dark:text-white relative z-10 px-6"
+          id="blogs"
+          onMouseEnter={() => setHoveredItem("blogs")}
+        >
           <Link href="/blogs" className="tracking-wide">
-            Blogs
+            <GiWhiteBook className="text-2xl" />
           </Link>
         </li>
-        <li className="min-h-12 flex items-center">
+        <li
+          className="min-h-12 flex items-center dark:text-white relative z-10 px-6"
+          id="contact"
+          onMouseEnter={() => setHoveredItem("contact")}
+        >
           <Link href="" className="tracking-wide h-full">
-            Contact
+            <AiFillPhone className="text-2xl" />
           </Link>
         </li>
-        <li className="min-h-12 flex items-center">
-          <ThemeButton className="text-xl" />
+        <li
+          className="min-h-12 flex items-center cursor-pointer dark:text-white relative z-10 pe-8 ps-6"
+          id="theme"
+          onMouseEnter={() => setHoveredItem("theme")}
+        >
+          <ThemeButton className="text-2xl cursor-pointer" />
         </li>
       </ul>
     </nav>
